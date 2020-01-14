8f277b1ca5053ed7375cfce76d887f89527810ec
liuzhanliang <zhanliangliu@qq.com>
2020-01-14 17:25:33 +0800

    learn rust

--- rust/hello-rust/src/main.rs
+++ rust/hello-rust/src/main.rs
@@ -11,4 +11,19 @@ fn main() {
 
     let logical: bool = true;
     println!("logical is {}", logical);
+
+    // Short-circuiting boolean logic
+    println!("true AND false is {}", true && false);
+    println!("true OR false is {}", true || false);
+    println!("NOT true is {}", !true);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("1 << 5 is {}", 1u32 << 5);
+    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
 }
